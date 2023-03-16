d904479fb90260248f84e0afd39ee5253a2b79c0
my-username123 <mwangikariuki672@gmail.com>
2023-03-16 16:40:35 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - use print f
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
