67b55d3c5433f908d03e92a522656a737e2bea7c
my-username123 <mwangikariuki672@gmail.com>
2023-03-24 19:46:55 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: input value
+ *
+ * Description: draw adiagonal line in the terminal
+ * Return: always (0)
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
