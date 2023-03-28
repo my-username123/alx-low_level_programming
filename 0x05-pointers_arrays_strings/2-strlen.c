6243478154b94b733de6ba55277ceafd30efbbc5
my-username123 <mwangikariuki672@gmail.com>
2023-03-28 12:17:46 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: input parameter
+ * Description: use pointers
+ * Return: always (0)
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; ++a)
+	{
+		continue;
+	}
+
+	return (a);
+}
+
