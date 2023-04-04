b19e28a1d22e92d50e1ab59952e024aa3fa2577c
my-username123 <mwangikariuki672@gmail.com>
2023-04-03 19:19:46 -0700

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - set the value of pointers to a char
+ *
+ * @s: double pointer that will be set to a char
+ * @to: pointer thats the value of cahr
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
