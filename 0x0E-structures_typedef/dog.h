b7d392d9d19144744a41ef76922660a4936c921e
my-username123 <mwangikariuki672@gmail.com>
2023-05-01 23:30:40 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,81 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog's main information
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The owner of the dog
+ *
+ * Description: No description for this struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                                                                                                  0,1           All
+
