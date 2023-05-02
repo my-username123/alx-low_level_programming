fad767240827a4cb4c0d9178638573813550a55a
my-username123 <mwangikariuki672@gmail.com>
2023-05-02 09:24:58 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a linked list
+ * @head: head of the linked list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
